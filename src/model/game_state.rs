590b60390972bc6149c616f376f6320a0da64aab
Addison Mink <addison.mink@example.com>
2025-05-30 15:28:33 -0500

    adding-npcs | update turn structure to include npcs.

--- src/model/game_state.rs
+++ src/model/game_state.rs
@@ -1,10 +1,14 @@
+use std::collections::VecDeque;
+
 use crate::util::*;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub enum GameState {
     Start,
     StartingTurn { time: f32 },
     SelectingMove { moves_left: u16 },
+    NpcSelectingMove,
     ExecutingMove { next_coord: Coord, moves_left: u16 },
+    NpcExecutingMove { path: VecDeque<Coord>, time: f32 },
     EndingTurn,
 }
--- src/update/mod.rs
+++ src/update/mod.rs
@@ -1,26 +1,40 @@
-use macroquad::prelude::trace;
+use std::collections::VecDeque;
 
 use crate::model::*;
 
 const TURN_START_DURATION: f32 = 1.0;
+const NPC_MOVE_DURATION: f32 = 0.5;
 
 pub fn update_game(game: &mut Game, delta_time: f32) -> Option<()> {
-    match game.state {
+    match game.state.clone() {
         GameState::Start => {
             game.state = GameState::StartingTurn {
                 time: TURN_START_DURATION,
             }
         }
         GameState::StartingTurn { time } => {
-            if time > 0.0 {
+            if time <= 0.0 {
+                let unit = game.active_unit()?;
+                if unit.is_player {
+                    game.state = GameState::SelectingMove {
+                        moves_left: unit.movement,
+                    };
+                } else {
+                    game.state = GameState::NpcSelectingMove;
+                }
+            } else {
                 game.state = GameState::StartingTurn {
                     time: time - delta_time,
                 };
+            }
+        }
+        GameState::NpcSelectingMove => {
+            let unit = game.active_unit()?;
+            if let Some(behavior) = unit.npc_behavior.as_ref() {
+                let path = (behavior.select_move)(unit, game);
+                game.state = GameState::NpcExecutingMove { path, time: 0.0 };
             } else {
-                let unit = game.active_unit()?;
-                game.state = GameState::SelectingMove {
-                    moves_left: game.active_unit()?.movement,
-                };
+                game.state = GameState::EndingTurn;
             }
         }
         GameState::SelectingMove { moves_left } => {
@@ -50,6 +64,30 @@ pub fn update_game(game: &mut Game, delta_time: f32) -> Option<()> {
                 game.state = GameState::SelectingMove { moves_left };
             } else {
                 game.state = GameState::EndingTurn;
+            }
+        }
+        GameState::NpcExecutingMove { path, time } => {
+            if path.is_empty() {
+                game.state = GameState::EndingTurn;
+            } else if time <= 0.0 {
+                let next_coord = path.front().copied().unwrap();
+                let unit = game.active_unit_mut()?;
+                let new_path: VecDeque<Coord> = path.iter().copied().skip(1).collect();
+
+                unit.coord = next_coord;
+                if unit.light.is_some() {
+                    game.light_grid = LightGrid::new(game);
+                }
+
+                game.state = GameState::NpcExecutingMove {
+                    path: new_path,
+                    time: NPC_MOVE_DURATION,
+                };
+            } else {
+                game.state = GameState::NpcExecutingMove {
+                    path,
+                    time: time - delta_time,
+                };
             }
         }
         GameState::EndingTurn => {
