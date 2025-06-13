1451d6aa45477ff000709e6dcd7486e7ce55a8d4
Addison Mink <addison.mink@example.com>
2025-06-13 15:52:12 -0500

    implement auto targeting.

--- src/level_system/state/selecting_target.rs
+++ src/level_system/state/selecting_target.rs
@@ -1,8 +1,6 @@
-use std::collections::HashMap;
-
-use macroquad::prelude::trace;
-
 use super::*;
+use macroquad::prelude::trace;
+use std::collections::HashMap;
 
 pub fn transition(level: &mut Level, action: Action) {
     match action.range {
@@ -18,6 +16,13 @@ fn single_unit_action(level: &mut Level, action: Action, min: u16, max: u16) {
 
     if targets.is_empty() {
         trace!("No valid targets found for action: {:?}", action);
+    }
+    // If there is only 1 target, select it automatically.
+    else if targets.iter().count() == 1 {
+        let target = targets.iter().next().unwrap();
+        let effects = action::compile_single_unit_action(level, &action, *entity, *target);
+        level.effect_queue.extend(effects);
+        level.state = LevelState::ResolvingAction;
     } else {
         let mut ts = HashMap::new();
         for target in targets {
